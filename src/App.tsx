fff4c8b98e2b3d5f885dc516f9a72e0118cd5d22
Geniroh2 <chibuzor@codematic.io>
2026-01-07 17:48:46 +0100

    welcome section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "#components/Navbar";
-import React from "react";
+import { Navbar, Welcome } from "#components/index";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
       App
     </main>
   );
